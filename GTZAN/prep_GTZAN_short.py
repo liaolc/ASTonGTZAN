00d397fa0272c3ad6c888eaf20197ecf919ac474
Lejun (Roy) Liao <112509497+liaolc@users.noreply.github.com>
2024-04-06 03:42:57 -0400

    Added run and prep iterations

---
+++ GTZAN/prep_GTZAN_short.py
@@ -0,0 +1,66 @@
+
+##Adapted from Yuan Gong (2021) -- yuangong@mit.edu 
+
+import numpy as np
+import json
+import os
+import wget
+# from google.colab import drive
+# drive.mount('/content/drive')
+# PATH_OF_DATA= '/content/gdrive/MyDrive/GTZAN/Data/'
+
+
+# prepare the data of the GTZAN dataset.
+print('Now download and process GTZAN dataset, it will take a few moments...')
+if os.path.exists('./data') == False:
+    os.mkdir('./data')
+# download the speechcommands dataset
+if os.path.exists('./data/GTZAN') == False:
+    # we use the 35 class v2 dataset, which is used in torchaudio https://pytorch.org/audio/stable/_modules/torchaudio/datasets/speechcommands.html
+    sc_url = 'https://www.dropbox.com/scl/fi/111zzafxe6pfky193o91x/GTZAN_short.tar.gz?rlkey=jyragsx4ymqgtiqe5fwznbcas&dl=1'
+    wget.download(sc_url, out='./data/')
+    print('Finished Download, Extracting')
+    os.mkdir('./data/GTZAN')
+    os.system('tar -xzvf ./data/GTZAN.tar.gz -C ./data/GTZAN')
+    #os.remove('./data/GTZAN.tar.gz')
+# Use training list from coreyker - "https://github.com/coreyker/dnn-mgr/tree/master/gtzan"
+with open('./data/GTZAN/Data/train_filtered.txt', 'r') as f:
+  train_list = f.readlines()
+with open('./data/GTZAN/Data/valid_filtered.txt', 'r') as f:
+  val_list = f.readlines()
+with open('./data/GTZAN/Data/test_filtered.txt', 'r') as f:
+  test_list = f.readlines()
+
+label_set = np.loadtxt('./data/GTZAN/Data/GTZAN_class_labels_indices.csv', delimiter=',', dtype='str')
+label_map = {}
+for i in range(1, len(label_set)):
+    label_map[eval(label_set[i][2])] = label_set[i][0]
+print(label_map)
+
+# generate  json files
+if os.path.exists('./data/datafiles') == False:
+    os.mkdir('./data/datafiles')
+    base_path = './data/GTZAN/Data/'
+    for split in ['test_filtered', 'valid_filtered', 'train_filtered']:
+        wav_list = []
+        with open(base_path+split+'.txt', 'r') as f:
+            filelist = f.readlines()
+        for file in filelist:
+            cur_label = label_map[file.split('/')[0]]
+            cur_path = os.path.abspath(os.getcwd()) + '/data/GTZAN/Data/' + file.strip()
+            cur_dict = {"wav": cur_path, "labels": '/m/gtzan'+cur_label.zfill(2)}
+            wav_list.append(cur_dict)
+        if split == 'train_filtered':
+            with open('./data/datafiles/GTZAN_train_data.json', 'w') as f:
+                json.dump({'data': wav_list}, f, indent=1)
+        if split == 'test_filtered':
+            with open('./data/datafiles/GTZAN_eval_data.json', 'w') as f:
+                json.dump({'data': wav_list}, f, indent=1)
+        if split == 'valid_filtered':
+            with open('./data/datafiles/GTZAN_valid_data.json', 'w') as f:
+                json.dump({'data': wav_list}, f, indent=1)
+        print(split + ' data processing finished, total {:d} samples'.format(len(wav_list)))
+
+    print('GTZAN dataset processing finished.')
+
+
