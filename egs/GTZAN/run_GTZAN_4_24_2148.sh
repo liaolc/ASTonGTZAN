f0a5c80dac1dbc2e26456ef45fd7c2b478b658fc
L Liao <lliao@Ls-MacBook-Air.local>
2024-04-24 22:22:43 -0400

    Added preprocessed audio wav files for GTZAN dataset

--- egs/GTZAN/run_GTZAN_4_24_2148.sh
+++ egs/GTZAN/run_GTZAN_4_24_2148.sh
@@ -13,17 +13,17 @@ set -x
 # comment this line if not running on sls cluster
 #. /data/sls/scratch/share-201907/slstoolchainrc
 # source ../../venvast/bin/activate
-export TORCH_HOME=../pretrained_models
+export TORCH_HOME=../../pretrained_models
 
 model=ast
 dataset=GTZAN
 imagenetpretrain=True
-audiosetpretrain=True
+audiosetpretrain=False
 bal=none
-lr=2.5e-5
+lr=1e-5
 epoch=10
 freqm=48
-timem=48
+timem=200
 mixup=0.6
 batch_size=4
 fstride=10
@@ -32,18 +32,25 @@ tstride=10
 dataset_mean=-6.845978
 dataset_std=5.5654526
 audio_length=1024
-noise=True
+noise=False
 
 metrics=acc
 loss=BCE
-warmup=False
+warmup=True
+wa=True
 lrscheduler_start=5
 lrscheduler_step=1
 lrscheduler_decay=0.85
 
-tr_data=./data/datafiles/GTZAN_train_data.json
-val_data=./data/datafiles/GTZAN_valid_data.json
-eval_data=./data/datafiles/GTZAN_eval_data.json
+
+#+ tr_data=/content/drive/MyDrive/Rana_Draytonii/Rana7/train_data.json
+#+ te_data=/content/drive/MyDrive/Rana_Draytonii/Rana7/val_data.json
+tr_data=/content/drive/MyDrive/ASTonGTZAN-main/egs/GTZAN/data/GTZAN_train_data.json
+val_data=/content/drive/MyDrive/ASTonGTZAN-main/egs/GTZAN/data/GTZAN_valid_data.json
+eval_data=/content/drive/MyDrive/ASTonGTZAN-main/egs/GTZAN/data/GTZAN_eval_data.json
+#tr_data=./data/datafiles/GTZAN_train_data.json
+#val_data=./data/datafiles/GTZAN_valid_data.json
+#eval_data=./data/datafiles/GTZAN_eval_data.json
 exp_dir=./exp/test-${dataset}-f$fstride-t$tstride-p$imagenetpretrain-b$batch_size-lr${lr}-decoupe
 
 if [ -d $exp_dir ]; then
@@ -54,7 +61,7 @@ mkdir -p $exp_dir
 
 # python ./prep_GTZAN.py
 
-CUDA_CACHE_DISABLE=1 python -W ignore ../src/run.py --model ${model} --dataset ${dataset} \
+CUDA_CACHE_DISABLE=1 python -W ignore ../../src/run.py --model ${model} --dataset ${dataset} \
 --data-train ${tr_data} --data-val ${val_data} --data-eval ${eval_data} --exp-dir $exp_dir \
 --label-csv ./data/GTZAN/Data/GTZAN_class_labels_indices.csv --n_class 10 \
 --lr $lr --n-epochs ${epoch} --batch-size $batch_size --save_model True \
